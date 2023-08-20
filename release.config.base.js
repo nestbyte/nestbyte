dd26d89d3d8f88df9679ea83072184ed56d11498
Aryansh Mahato <contact@aryansh.dev>
2023-08-20 11:11:21 +0530

    refactor(setup): add pre release tag GH-1

--- release.config.base.js
+++ release.config.base.js
@@ -1,5 +1,5 @@
 const config = {
-  branches: ['main'],
+  branches: [{ name: 'main', prerelease: true }],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',

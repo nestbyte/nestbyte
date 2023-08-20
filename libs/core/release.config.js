17163c5cfeac09cad7213e1251fd8388c0d16dea
Aryansh Mahato <contact@aryansh.dev>
2023-08-20 11:09:44 +0530

    refactor(setup): fix release config GH-1

--- libs/core/release.config.js
+++ libs/core/release.config.js
@@ -1,21 +1,27 @@
 const name = 'core';
 const srcRoot = `libs/${name}`;
 
-const config = [
-  [
-    '@semantic-release/changelog',
-    {
-      changelogFile: `${srcRoot}/CHANGELOG.md`,
-    },
+const config = {
+  extends: 'release.config.base.js',
+  pkgRoot: `dist/${srcRoot}`,
+  tagFormat: name + '-v${version}',
+  commitPaths: [`${srcRoot}/*`],
+  plugins: [
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${srcRoot}/CHANGELOG.md`,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [`${srcRoot}/CHANGELOG.md`, `${srcRoot}/package.json`],
+        message: `release(${name}): $\{nextRelease.version} [skip ci]\n\n$\{nextRelease.notes}`,
+      },
+    ],
+    '@semantic-release/npm',
   ],
-  [
-    '@semantic-release/git',
-    {
-      assets: [`${srcRoot}/CHANGELOG.md`, `${srcRoot}/package.json`],
-      message: `release(${name}): $\{nextRelease.version} [skip ci]\n\n$\{nextRelease.notes}`,
-    },
-  ],
-  '@semantic-release/npm',
-];
+};
 
 module.exports = config;
